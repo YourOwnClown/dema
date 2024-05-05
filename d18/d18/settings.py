a11c2c58e24916db5104e93bcadd8d0df296d59a
Admin <hjk>
2024-05-05 17:21:05 +1000

    d29

--- d18/d18/settings.py
+++ d18/d18/settings.py
@@ -132,3 +132,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #users
 AUTH_USER_MODEL = 'users.user'
+
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'doyt2010@yandex.ru'
+EMAIL_HOST_PASSWORD = 'qrepntaenmkiltkw'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
--- d18/d18/urls.py
+++ d18/d18/urls.py
@@ -1,4 +1,4 @@
-
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path,include
 from products.views import *
@@ -6,10 +6,18 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('',index, name = 'index'),
     path('products/', include('products.urls', namespace ='products')),
-    path('users/', include('users.urls', namespace ='users'))
+    path('users/', include('users.urls', namespace ='users')),
+    path('admin/password_reset/', auth_views.PasswordResetView.as_view(),
+         name = 'admin_password_reset'),
+    path('admin/password_reset/done', auth_views.PasswordResetDoneView.as_view(),
+         name = 'password_reset_done'),
+    path('admin/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name = 'password_reset_confirm'),
+    path('admin/reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+         name = 'password_reset_complete'),
+    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG == True:
